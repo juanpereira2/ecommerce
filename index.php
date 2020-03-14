08eba11200ea0deae7677cd3a9db469e3f9e6b60
juanpereira2 <juanpereira74@hotmail.com>
2020-03-13 21:51:47 -0300

    updated create route

--- index.php
+++ index.php
@@ -107,21 +107,27 @@ $app->get("/admin/users/:iduser", function ($iduser) {
 });
 
 $app->post("/admin/users/create", function () {
+
 	User::verifyLogin();
 
-
-	$user = new User();
+   $user = new User();
 
 	$_POST["inadmin"] = (isset($_POST["inadmin"])) ? 1 : 0;
 
+	$_POST['despassword'] = password_hash($_POST["despassword"], PASSWORD_DEFAULT, [
+
+		"cost"=>12
+
+	]);
+
 	$user->setData($_POST);
-	$user->save();
-	//var_dump ($user);
 
-	header("Location: /ecommerce/admin/users/");
+   $user->save();
+
+   header("Location: /admin/users");
 	exit;
-});
 
+});
 $app->post("/admin/users/:iduser", function ($iduser) {
 	User::verifyLogin();
 	$user = new User ();
